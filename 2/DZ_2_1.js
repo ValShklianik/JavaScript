98335f158cae31949c5be855ed6b3989507fe9a7
ValShklianik <Val.Shklianik@gmail.com>
2017-03-27 17:25:46 +0300

    2.dz.1&2

---
+++ 2/DZ_2_1.js
@@ -0,0 +1,31 @@
+/*обаратиться к каждому элементу массива 
+и рандомно его заполнить числами умнож от с на р, среднее ар, отсортировать*/
+var n = Number(prompt("введите кол-во элементов массива"));
+var s = Number(prompt("введите значения элементов"));
+var p = Number(prompt("ведите процент отклонения"));
+var p = p/100;
+var sum = 0;
+var arr = [];
+var avg = 0;
+
+for (var i = 0; i <= n; i++) {
+	arr[i] = Math.random()*((s + s * p) - (s - s * p) + 1) + (s - s * p);
+	console.log(arr[i]);
+}
+
+
+for (i = 0; i <= n; i++) {
+            avg = avg + arr[i];
+        }
+
+ avg = avg / arr.length;
+
+console.log("среднее арифметическое:" + avg);
+
+
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+console.log('Сортировка:', arr.sort(compareNumbers));
---
+++ 2/DZ_2_2.js
@@ -0,0 +1,10 @@
+﻿var date = new Date();
+var end;
+
+console.log(date);
+
+var hrs = 24 - date.getHours();
+var min = 60 - date.getMinutes();
+var sec = 60 - date.getSeconds();
+
+console.log("До конца дня еще: " + hrs + " часов " + min + " минуток " + sec + " секундок "); 
