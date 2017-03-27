77115c2156e02b88945522ea2d91da316b13d5c4
ValShklianik <Val.Shklianik@gmail.com>
2017-03-28 00:00:54 +0300

    zad.3

---
+++ 2/DZ_2_3.js
@@ -0,0 +1,13 @@
+﻿var n = prompt("Введите n: ");
+var x = prompt("Введите x: ");
+
+var arr = [];
+var res = 0;
+
+for(var i = 0; i <= n; i++){
+
+	arr[i] = prompt("Введите а "+ i);
+	res = res + arr[i]*Math.pow(x, i);
+	
+}
+console.log("Результат вычисления= " + res);
