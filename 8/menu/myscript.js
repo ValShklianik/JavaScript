95722c61ee9ff42cdc4d522e8c04532576fd53e9
Val <val.shklianik@gmail.com>
2017-06-12 20:25:54 +0300

    add

--- 8/menu/myscript.js
+++ 8/menu/myscript.js
@@ -2,7 +2,7 @@ var li=document.getElementsByTagName('li');
 for (var i = 0; i < li.length; i++) {
 	li[i].addEventListener('click',function(e){
 		e.stopPropagation;
-
+		
 		var arr=this.parentNode.children;
 		for (var i = 0; i < arr.length; i++) {
 			if(arr[i].classList.contains('active')){
